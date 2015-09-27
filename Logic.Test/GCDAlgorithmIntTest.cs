2c30c6fefd206def509b5928e553cd03e26da5d2
Alexander-Ryzhkov <alexander.ryzhkov.96@gmail.com>
2015-09-27 14:28:04 +0300

    Added euclid & binary algorithm for 2 args, tests

---
+++ Logic.Test/GCDAlgorithmIntTest.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Logic.Test
+{
+    [TestClass]
+    public class GCDAlgorithmIntTest
+    {
+        //
+        //
+        //  Testing Euclid
+        //
+        //
+        [TestMethod]
+        public void EuclidTest_PositiveInts()
+        {
+            int arrange = 14;
+            int x = 182, y = 238;
+
+            int act = GCDAlgorithmInt.Euclid(x, y);
+            Assert.AreEqual(arrange, act);
+        }
+        
+        [TestMethod]
+        public void EuclidTest_NegativeInts()
+        {
+            int arrange = 14;
+            int x = -182, y = -238;
+
+            int act = GCDAlgorithmInt.Euclid(x, y);
+
+            Assert.AreEqual(arrange, act);
+
+        }
+        
+        [TestMethod]
+        public void EuclidTest_EqualInts()
+        {
+            int arrange = 19;
+            int x = 19, y = 19;
+
+            int act = GCDAlgorithmInt.Euclid(x, y);
+
+            Assert.AreEqual(arrange, act);
+
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(OverflowException))]
+        public void EuclidTest_OverflowException()
+        {
+            GCDAlgorithmInt.Euclid(int.MinValue, 0);
+        }
+
+
+        //
+        //
+        //  Testing Binary
+        //
+        //
+        [TestMethod]
+        public void BinaryTest_PositiveInts()
+        {
+            int arrange = 7;
+            int x = 91, y = 238;
+
+            int act = GCDAlgorithmInt.Binary(x, y);
+            Assert.AreEqual(arrange, act);
+        }
+
+        [TestMethod]
+        public void BinaryTest_NegativeInts()
+        {
+            int arrange = 14;
+            int x = -182, y = -238;
+
+            int act = GCDAlgorithmInt.Binary(x, y);
+
+            Assert.AreEqual(arrange, act);
+
+        }
+
+        [TestMethod]
+        public void BinaryTest_EqualInts()
+        {
+            int arrange = 19;
+            int x = 19, y = 19;
+
+            int act = GCDAlgorithmInt.Binary(x, y);
+
+            Assert.AreEqual(arrange, act);
+
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(OverflowException))]
+        public void BinaryTest_OverflowException()
+        {
+            GCDAlgorithmInt.Binary(int.MinValue, 0);
+        }
+    }
+}
---
+++ Logic/GCDAlgorithmInt.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    public class GCDAlgorithmInt
+    {
+
+
+        public static int Euclid(int x, int y)
+        {
+            x = Math.Abs(x);
+            y = Math.Abs(y);
+
+            if (x == y) return x;
+            if (x == 0) return y;
+            if (y == 0) return x;
+
+            if (x < y)
+                return Euclid(x, y % x);
+
+            return Euclid(x % y, y);
+        }
+
+        public static int Euclid(int x, int y, out long time)
+        {
+            int result;
+            Stopwatch timer = Stopwatch.StartNew();
+
+            result = Euclid(x, y);
+
+            timer.Stop();
+            time = timer.ElapsedTicks;
+
+            return result;
+        }
+
+
+
+        public static int Binary(int x, int y)
+        {
+            x = Math.Abs(x);
+            y = Math.Abs(y);
+
+            if (x == y) return x;
+            if (x == 0) return y;
+            if (y == 0) return x;
+
+            if (x % 2 == 0)
+            {
+                if (y % 2 == 0)
+                    return 2 * Binary(x / 2, y / 2);
+                else
+                    return Binary(x / 2, y);
+            }
+
+            if (y % 2 == 0)
+                return Binary(x, y / 2);
+
+            if (x > y)
+                return Binary((x - y) / 2, y);
+
+            return Binary(x, (y - x) / 2);
+        }
+
+        public static int Binary(int x, int y, out long time)
+        {
+            int result;
+            Stopwatch timer = Stopwatch.StartNew();
+
+            result = Binary(x, y);
+
+            timer.Stop();
+            time = timer.ElapsedTicks;
+
+            return result;
+        }
+
+    }
+}
